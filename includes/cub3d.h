ed5151d0b23fd34e70a732dbedee0075f10290aa
keonwoo98 <keonwoo98@naver.com>
2022-02-08 10:24:33 +0900

    Add : cub3d.h

---
+++ includes/cub3d.h
@@ -0,0 +1,130 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#include "../minilibx/mlx.h"
+#include "../Libft/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <fcntl.h>
+
+# define PI 3.14159265359
+
+# define mapWidth 24
+# define mapHeight 24
+# define screenWidth 640
+# define screenHeight 480
+
+// # define ROW 11
+// # define COL 15
+// # define TILE_SIZE 32
+// # define WIDTH COL * TILE_SIZE
+// # define HEIGHT ROW * TILE_SIZE
+# define T_SIZE 30
+
+# define KEY_ESC 53
+# define KEY_EVENT_PRESS 2
+# define KEY_EVENT_EXIT 17
+# define KEY_W 13
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
+# define KEY_LEFT 123
+# define KEY_RIGHT 124
+
+# define RED 0xFF0000
+# define GREEN 0x00FF00
+# define BLUE 0x0000FF
+# define BLACK 0x000000
+# define WHITE 0xFFFFFF
+# define GRAY 0x007F7F7F
+# define GOLD 0x00FFd700
+# define SILVER 0xC0C0C0
+
+typedef struct s_vec
+{
+    double x;
+    double y;
+}t_vec;
+
+typedef struct s_ray
+{
+	double posX;
+	double posY;	// 플레이어의 초기 위치벡터
+	double dirX;
+	double dirY;	// 플레이어의 초기 방향벡터
+	double planeX;	// 플레이어의 카메라 평면
+	double planeY;	// FOV는 "카메라 평면의 길이 : 방향벡터의 길이"의 비율로 결정됨
+	double time;	// 현재 프레임 시간
+	double oldTime;	// 이전 프레임 시간
+
+    // t_vec dir;
+    // t_vec side_vec;
+    // t_vec delta;
+    // int step_x;
+    // int step_y;
+    // int side;
+    // double wall_dist;
+    // int line_height;
+    // int map_x;
+    // int map_y;
+    // int draw_start;
+    // int draw_end;
+    // int collision_dir;
+}t_ray;
+
+typedef struct s_info
+{
+	void *mlx;
+	void *win;
+    int map[mapWidth][mapHeight];
+	// int **map;
+	void *img;
+	int *data;
+	int bpp;
+	int line_size;
+	int endian;
+	int win_width;
+	int win_height;
+	int bmp;
+    t_ray ray;
+    t_vec dir_vec;
+}t_info;
+
+/*
+0 : 빈 공간
+1 : 벽
+2 : 내부의 작은 방
+3 : 몇 개의 기둥
+4 : 복도
+*/
+int testMap[mapWidth][mapHeight] =
+{
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+#endif
