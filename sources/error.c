ff50510470f0ab55593ecd48c4a87bba6f83e8c9
keonwoo98 <keonwoo98@naver.com>
2022-03-30 22:25:28 +0900

    Fix: Norminette v3

--- sources/error.c
+++ sources/error.c
@@ -6,7 +6,7 @@
 /*   By: keokim <keokim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 10:20:19 by keokim            #+#    #+#             */
-/*   Updated: 2022/03/30 10:20:23 by keokim           ###   ########.fr       */
+/*   Updated: 2022/03/30 22:16:49 by keokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,29 @@ void
 {
 	perror(err_msg);
 	exit(EXIT_FAILURE);
+}
+
+void
+	free_exit_error(char **str, char *err_msg)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		free(str[i++]);
+	free(str);
+	ft_putstr_fd(err_msg, 2);
+	exit(EXIT_FAILURE);
+}
+
+int
+	free_return_error(char **str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		free(str[i++]);
+	free(str);
+	return (RETURN_FAILURE);
 }
